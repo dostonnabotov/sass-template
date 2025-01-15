0932a4661868485a5ef1ed30169e489fe0c8eda7
Doston Nabotov <dostonnabotov@gmail.com>
2025-01-15 09:16:13 +0200

    Fix vite version and typo

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/sass-template/",
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler", // or "modern"
-      },
-    },
-  },
-});
+export default defineConfig({});
